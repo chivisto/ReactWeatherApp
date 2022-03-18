80c3ad790871897ceb0a50eda08d8ef1df35f7fb
Chivisto <isaaccovarr@gmail.com>
2022-03-18 15:03:23 -0600

    Formatting

--- weatherapp/src/App.js
+++ weatherapp/src/App.js
@@ -1,25 +1,34 @@
 import React, { useState, useEffect } from 'react';
 
-
 function App() {
+  const api = {
+    key: "&appid=1852e77cf37dc313345e20cf7c16fee9",
+    url: "http://api.openweathermap.org/data/2.5/"
+  }
+
   const [input, setInput] = useState(null)
   const [search, setSearch] = useState(false)
+  const [weather, setWeather] = useState([]);
 
   function getInput(val){
     setInput(val.target.value)
     setSearch(false)
-    console.log(val.target.value)
   }
 
   useEffect(() => {
     async function getApi(){
-      const url ="http://api.openweathermap.org/data/2.5/weather?q=&appid=1852e77cf37dc313345e20cf7c16fee9";
-      const data = await url.json();
+      const resp = await fetch (`${api.url}weather?&q=${input}&units=metric${api.key}`);
+      const data = await resp.json();
   
       console.log(data.results)
-  
+      setWeather(data.results);
     }
-  });
+    getApi()
+  }, []);
+
+  console.log(weather)
+
+
 
   return (
   <div className='App'>
